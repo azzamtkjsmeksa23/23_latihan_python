ff5deaab8b42fe6eb132dde1f9dc9b4c16bc03d9
Nona Nano <156498581+NonaNano1@users.noreply.github.com>
2026-08-10 10:50:43 +0700

    baru nich

--- ganjilgenap.py
+++ ganjilgenap.py
@@ -1,5 +1,12 @@
-x = int(input("Masukkan angkah: "))
-if x % 2 == 0:
-    print("genap")
-else:
-    print("ganjil")
+while True:
+    x = int(input("Masukkan angkah: "))
+    if x % 2 == 0:
+        print("itu adalah angka genap")
+    else:
+        print("itu adalah angka ganjil")
+    jalan = str(input('masih lanjyut? (y/n): '))
+    if jalan == "y":
+        continue
+    else:
+        print("Terima Kasih💖!!")
+        break
