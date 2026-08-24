0a47f9e45081c76aa7beca5a812bb038b2459d09
23_Muhammad Abdullah Ammar Azzam Izzuddin <azzam.tkjsmeksa25@gmail.com>
2026-08-24 09:03:33 +0700

    commit kelima

--- ganjilgenap.py
+++ ganjilgenap.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 while True:
     x = int(input("Masukkan angkah: "))
     if x % 2 == 0:
@@ -13,17 +12,3 @@ while True:
         break
 
 print('hai')
-=======
-while True:
-    x = int(input("Masukkan angkah: "))
-    if x % 2 == 0:
-        print("itu adalah angka genap")
-    else:
-        print("itu adalah angka ganjil")
-    jalan = str(input('masih lanjyut? (y/n): '))
-    if jalan == "y":
-        continue
-    else:
-        print("Terima Kasih💖!!")
-        break
->>>>>>> 3a496c138f94c842c0a0b51f66a1de5af62f3ca9
