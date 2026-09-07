dbd5f9f1794eaebd6dc4afbc7666a53e8b92dec8
Nona Nano <156498581+NonaNano1@users.noreply.github.com>
2026-09-07 12:40:50 +0700

    commit ke sekian 5

--- main.py
+++ main.py
@@ -3,25 +3,31 @@ import modul_matematika as mm
 import modul_BangunRuang as Mb
 import autentikasi as aut
 
-print(aut.main_menu())
+while not aut.login():
+    print('Login dulu Dek 😂😝')
+    print(aut.main_menu())
 
-print("Pilih rumus matematika\n1. ganjil genap\n2. Bilangan prima\n3. tambah kali kurang bagi\n4. Keliling Persegi\n5. Luas Segitiga")
+print("\nPilih rumus matematika\n1. ganjil genap\n2. Bilangan prima\n3. tambah kali kurang bagi\n4. Keliling Persegi\n5. Luas Segitiga\n6. Keluar")
 
-maumana = input("mau yang mana: ")
-if maumana == '1':
-    gj = print()
-    gg.ganjil_genap(gj)
-elif maumana == '2':
-    ya = int(input("masukkan angka: "))
-    mm.bilangan_prima(ya)
-elif maumana == '3':
-    tak = int(input('masukkan angka 1: '))
-    kul = int(input('masukkan angka 2: '))
-    mm.tamkuli(tak, kul)
-elif maumana == '4':
-    s = int(input("Masukkan Angkah: "))
-    Mb.keliling_persegi(s)
-elif maumana == '5':
-    a = float(input('Masukkan Alas Segitiga: '))
-    t = float(input('Masukkan Tinggi Segitiga: '))
-    Mb.luas_segitiga(a, t)
+while True:
+    maumana = input("mau yang mana: ")
+    if maumana == '1':
+        gj = print()
+        gg.ganjil_genap(gj)
+    elif maumana == '2':
+        ya = int(input("masukkan angka: "))
+        mm.bilangan_prima(ya)
+    elif maumana == '3':
+        tak = int(input('masukkan angka 1: '))
+        kul = int(input('masukkan angka 2: '))
+        mm.tamkuli(tak, kul)
+    elif maumana == '4':
+        s = int(input("Masukkan Angkah: "))
+        Mb.keliling_persegi(s)
+    elif maumana == '5':
+        a = float(input('Masukkan Alas Segitiga: '))
+        t = float(input('Masukkan Tinggi Segitiga: '))
+        Mb.luas_segitiga(a, t)
+    elif maumana == '6':
+        print('Terima Kasih 💖')
+        break
